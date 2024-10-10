b47804ea5b6e0c1e37e7a4b07cdc3cbcf53da392
Aleix Lahoz <aleix.lahoz@m47labs.com>
2024-10-10 18:02:41 +0200

    implementation of GladiaSTT as function in function app

--- AI_Knowledge/STT_TTS_Evaluation/05_run_stt_evaluation.py
+++ AI_Knowledge/STT_TTS_Evaluation/05_run_stt_evaluation.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Configuration
-connection_string = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+connection_string = os.getenv('FUNCTIONAPP_AZURE_STORAGE_CONNECTION_STRING')
 table_client = TableServiceClient.from_connection_string(connection_string).get_table_client(configuration.table_name_english)
 
 # Set the desired concurrency limit
@@ -16,8 +16,8 @@ MAX_CONCURRENT_INVOCATIONS = 10  # Adjust this number as needed
 def invoke_function(row):
     row_id = row['RowKey']  # Adjust this based on your table schema
     file_url = row['file_url']  # Get the file URL
-    function_url = f"https://{configuration.function_app_name}.azurewebsites.net/api/{configuration.function_name_gladiastt}?row_id={row_id}&file_url={file_url}"
-
+    partition_key = row['PartitionKey']
+    function_url = f"https://{configuration.function_app_name}.azurewebsites.net/api/{configuration.function_name_gladiastt}?row_id={row_id}&file_url={file_url}&partition_key={partition_key}"
     response = requests.post(function_url)
     return response.json()
 
